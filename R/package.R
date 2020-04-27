5f6ece824bd1e06a6de4bca1dbbd5c53dad6af49
markklik <markklik@gmail.com>
2020-04-27 17:31:53 +0200

    Change license to MIT

--- R/package.R
+++ R/package.R
@@ -1,20 +1,24 @@
 #  lazyplyr - a most lazy dplyr implementation for remote datasets
 #
-#  Copyright (C) 2019-present, Mark AJ Klik
+# Copyright (c) 2020 M.A.J. Klik
 #
-#  This file is part of the lazyplyr R package.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#  The lazyplyr R package is free software: you can redistribute it and/or modify it
-#  under the terms of the GNU Affero General Public License version 3 as
-#  published by the Free Software Foundation.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#  The lazyplyr R package is distributed in the hope that it will be useful, but
-#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
-#  for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License along
-#  with the lazyplyr R package. If not, see <http://www.gnu.org/licenses/>.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 #  You can contact the author at:
 #  - lazyplyr R package source repository : https://github.com/fstpackage/lazyplyr
